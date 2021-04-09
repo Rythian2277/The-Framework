6bcd031f0e051287aad6c3d5f19ffca34a5495a5
Rythian Smythe <rythian@rythian2277.dev>
2021-04-09 03:10:06 +0100

    Upload progress

---
+++ Source/Server/AnimationHandler.lua
@@ -0,0 +1,11 @@
+return {
+    --// Fires every time Framework:Connect(player) is called
+    Connect = function(framework, player)
+
+    end,
+    --// Fires every time Framework:Disconnect(player) is called
+    Disconnect = function(framework, player)
+
+    end,
+    
+}
